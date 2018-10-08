bdaf3e81ccf0bb6193eb57024c4286beb0936536
BeatHubmann <BeatHubmann@users.noreply.github.com>
2018-10-08 06:51:09 +0200

    Work on task3

--- ex02/task3.cpp
+++ ex02/task3.cpp
@@ -62,8 +62,8 @@ void OutputLattice(int lattice[], const int size, const int time_step)
 {
     char cmd[160], filename[160], filename_png[160];
 
-    sprintf(filename, "task2_%03d.ppm", time_step);
-    sprintf(filename_png, "task2_%03d.png", time_step);
+    sprintf(filename, "task3_%03d.ppm", time_step);
+    sprintf(filename_png, "task3_%03d.png", time_step);
     sprintf(cmd, "convert %s %s; rm -f %s", filename, filename_png, filename);  
 
     Print_lattice (lattice, size, size, ImageWidth, ImageHeight, filename);
@@ -71,20 +71,16 @@ void OutputLattice(int lattice[], const int size, const int time_step)
     system(cmd);
 }
 
-std::tuple<bool, bool> TimeStep(int lattice[], const int size, const int time_step, const bool generate_images)
+std::tuple<bool, bool> TimeStep(int lattice[], const int size, const int time_step)
 {
     bool fire_alive= false;
     bool reached_opposite= false;
-    if (time_step % 10 == 0) std::cout << "..." <<std::endl;
-    int image_lattice[size * size]= {0};
 
     #pragma omp parallel for 
     for (int ij= 0; ij < size*size; ij++)
     {
         if (lattice[ij] == time_step-1)
         {
-            image_lattice[ij]= 3;
-
             std::vector<int> neighbors;
             GetNeighbors(lattice, size, ij, neighbors);
             if (neighbors.size() != 0)
@@ -93,59 +89,70 @@ std::tuple<bool, bool> TimeStep(int lattice[], const int size, const int time_st
             {
                 if (neighbor / size == (size-1)) reached_opposite= true;
                 lattice[neighbor]= time_step;
-                image_lattice[neighbor]= 2;
             }
         }
-        else if (lattice[ij] == 1) image_lattice[ij]= 1;
-        else if (lattice[ij] < time_step-1 && lattice[ij] > 1) image_lattice[ij]= 3;
     }
-
-    if (generate_images) OutputLattice(image_lattice, size, time_step);
-
     return std::make_tuple(fire_alive, reached_opposite); 
 }
 
-int main(int argc, char* argv[])
-{
-    if (argc < 5) // check command line arguments and give some help
-     {  
-        std::cerr << "Usage: " << argv[0] << " float_site_occupation_probabilty int_lattice_side_length int_random_seed bool_print_images\n" << std::endl;
-        return 1;
-     }
 
-    const double treshold= atof(argv[1]);
-    const int size= atoi(argv[2]);
-    const int seed= atoi(argv[3]);
-    const bool generate_images= atoi(argv[4]);
-    int lattice[size * size]= {0};
+auto RunExperiment(const int size, const double treshold, const int seed)
+{
+    std::cout << "Running exp " << seed << "\t" << treshold << std::endl;
+    int lattice[size * size]{0};
 
     PopulateLattice(lattice, size, treshold, seed);
 
     bool fire_alive= false;
-    bool reached_opposite= false;
 
     int time_step= 2;
-    int shortest_path= std::numeric_limits<int>::max();
-    std::cout << std::left 
-              << std::setw(30) << "Lattice side length: " << std::setw(15) << size << std::endl
-              << std::setw(30) << "Occupation probability: " << std::setw(15) << treshold << std::endl
-              << std::setw(30) << "Random seed: " << std::setw(15) << seed << std::endl
-              << std::setw(30) << "Calculating..." << std::endl;
+    int shortest_path= 0;
+    bool spanning_cluster= false;
 
     fire_alive= StartFire(lattice, size);
-    if (generate_images) Print_lattice (lattice, size, size, ImageWidth, ImageHeight, "task2_002.png");    
 
     while (fire_alive)
     {
         time_step++;
-        auto step_result= TimeStep(lattice, size, time_step, generate_images);
+        auto step_result= TimeStep(lattice, size, time_step);
         fire_alive= std::get<0>(step_result);
-        reached_opposite= std::get<1>(step_result);
-        if (reached_opposite) shortest_path= std::min(time_step-1, shortest_path);
+        if (!(spanning_cluster) && std::get<1>(step_result));
+            shortest_path= time_step -1;
     }
+    const int fire_duration= time_step - 2;
 
-    std::cout << std::left
-              << std::setw(30) << "Fire duration:" << std::setw(15) << time_step-2 << std::endl
-              << std::setw(30) << "Shortest path:" << std::setw(15) << (shortest_path < std::numeric_limits<int>::max() ? shortest_path : -1) << std::endl;
+    return std::make_tuple(spanning_cluster, fire_duration, shortest_path);
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+    std::vector<double> tresholds{0.1, 0.5, 0.9};
+    const int size= 100;
+    const int initial_seed= 42;
+    const int num_samples= 1000;
+    
+    for (auto treshold : tresholds)
+    {
+        int num_spanning_clusters= 0;
+        int total_fire_duration= 0;
+        int total_shortest_path= 0;
+        for (auto n= 0; n < num_samples; n++)
+        {
+            auto result= RunExperiment(size, treshold, initial_seed + n);
+            num_spanning_clusters += std::get<0>(result);
+            total_fire_duration += std::get<1>(result);
+            total_shortest_path += std::get<2>(result);
+        }
+        std::cout << "Done!" << std::endl;
+        std::cout << treshold << "\t"
+                  << num_spanning_clusters / num_samples << "\t"
+                  << total_fire_duration / num_samples << "\t"
+                  << total_shortest_path / num_spanning_clusters << std::endl;
+    }
     return 0;
 }
+
+
