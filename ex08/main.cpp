7b533aa9599c9619f16a71dc31124f397f7e0065
BeatHubmann <BeatHubmann@users.noreply.github.com>
2018-11-19 15:47:42 +0100

    Finish ex08

--- ex08/main.cpp
+++ ex08/main.cpp
@@ -5,10 +5,9 @@
 #include <cmath>
 #include <cassert>
 
-double SquareDistance(const std::vector<double> a,
-                      const std::vector<double> b=std::vector<double>(3, 0.0))
+double SquareDistance(const std::array<double, 3> a,
+                      const std::array<double, 3> b = std::array<double, 3>{0.0, 0.0, 0.0})
 {
-    assert (a.size() == b.size());
     double square_dist{0.0};
     for (auto i= 0; i < a.size(); i++)
     {
@@ -18,20 +17,20 @@ double SquareDistance(const std::vector<double> a,
     return square_dist;
 }
 
-double AbsDistance(const std::vector<double> a,
-                   const std::vector<double> b=std::vector<double>(3, 0.0))
+double AbsDistance(const std::array<double, 3> a,
+                   const std::array<double, 3> b=std::array<double, 3>{0.0, 0.0, 0.0})
 {
     return std::sqrt(SquareDistance(a, b));
 }
 
-bool Overlap(const std::vector<double> a, const std::vector<double> b,
+bool Overlap(const std::array<double, 3> a, const std::array<double, 3> b,
              const double r)
 {
     return (AbsDistance(a, b) < r);
 }
 
-bool OverlapChain(const std::vector<std::vector<double>>& chain,
-                  const std::vector<double>& pos_new,
+bool OverlapChain(const std::vector<std::array<double, 3>>& chain,
+                  const std::array<double, 3>& pos_new,
                   const double r)
 {
     bool overlap{false};
@@ -46,48 +45,49 @@ bool OverlapChain(const std::vector<std::vector<double>>& chain,
     return overlap;
 }
 
-
-
 double RandomWalk(const int N, const int seed, const bool self_avoid=false,
-                  const bool spherical=false,
-                  const double segment_length=1.0,
-                  const double r= 0.0)
+                  const bool spherical=false,          // walk in 3D i.s.o. 2D
+                  const double segment_length=1.0,     // polar coordinates
+                  const double r= 0.0,                 // particle radius
+                  const int max_attempts= 100)         // when to give up if stuck
 {
     std::mt19937 rd(seed);
     std::uniform_real_distribution<> angle(0, M_PI);
-    std::vector<std::vector<double>> chain;
-    chain.reserve(N);
-    chain.push_back(std::vector<double>(3, 0.0));
-    std::vector<double> pos_new(3, 0.0);
-    double phi{0.0};
-    double theta{M_PI_2};
-    double sin_theta{1.0}, cos_theta{0.0};
+    std::vector<std::array<double, 3>> chain; 
+    chain.reserve(N); // reserve memory space for efficiency
+    std::array<double, 3> pos_new{0.0, 0.0, 0.0}; // place holder for walk steps
+    chain.push_back(pos_new); // use origin as starting position
+    double phi{0.0}; // polar coordinates
+    double theta{M_PI_2}; // polar coordinates
+    double sin_theta{1.0}, cos_theta{0.0}; // precalculate for 2D and/or efficiency
+    int attempts{0}; // for tracking attempts when getting stuck
 
     while (chain.size() < N)
     {
-        phi= 2 * angle(rd);
-        if (spherical)
+        phi += 2 * angle(rd);
+        if (spherical) // in case we're in 3D (as in task 2)
         {
-            theta= angle(rd);
+            theta += angle(rd);
             sin_theta= std::sin(theta);
             cos_theta= std::cos(theta);
         }
         pos_new[0]= chain.back()[0] + segment_length * sin_theta * std::cos(phi);
         pos_new[1]= chain.back()[1] + segment_length * sin_theta * std::sin(phi);
         pos_new[2]= chain.back()[2] + segment_length * cos_theta;
-        if (!self_avoid || !OverlapChain(chain, pos_new, r))
+        if (!self_avoid || !OverlapChain(chain, pos_new, r)) // either no collision or don't care
         {
-            // std::cout.precision(4);
-            // std::cout << "Segment no. " << chain.size() << "\t\t"
-            //           << "x:\t"  << pos_new[0] << "\t\t"
-            //           << "y:\t"  << pos_new[1] << "\t\t"
-            //           << "z:\t"  << pos_new[2] << "\t\t"
-            //           << "Dst:\t"<< AbsDistance(chain.back(), pos_new) << "\t\t"
-            //           << "Ttl:\t"<< AbsDistance(pos_new) << std::endl;
-            chain.push_back(pos_new);
+            chain.push_back(pos_new); // add particle to chain
+            attempts= 0; // reset attempts in case we were probing for new position
+        }
+        else // we are self-avoiding and have a collision
+        {
+            if (attempts++ == max_attempts) break; // seems we're trapped - abort this walk
         }
     }
-    return SquareDistance(chain.back());
+    if (chain.size() == N) // successful walk
+        return SquareDistance(chain.back()); // return length
+    else // aborted walk because we got stuck 
+        return -1.0; // error value
 }
 
 void RunExp1(const int N, const int M, const int seed,
@@ -98,23 +98,30 @@ void RunExp1(const int N, const int M, const int seed,
 {
     double sum_R_squared{0.0};
     double sum_R_squared_squared{0.0};
-    for (auto k= 1; k <= M; k++)
+    int k{1};
+    int seed_shift{0};
+    while (k < M)
     {
-        const double ThisWalk{RandomWalk(N, seed + k,
+        const double ThisWalk{RandomWalk(N, seed + seed_shift,
                                          self_avoid,
                                          spherical,
                                          segment_length,
                                          r)};
-        sum_R_squared += ThisWalk;
-        sum_R_squared_squared += ThisWalk * ThisWalk;
-        if (k % 10 == 0) // output every tenth step
+        seed_shift++;
+        if (ThisWalk > 0)
         {
-            const double ens_avg_R_squared{sum_R_squared / k};
-            std::cout << k << "\t"
-                      << ens_avg_R_squared << "\t"
-                      << std::sqrt(((sum_R_squared_squared / k)
-                                     - ens_avg_R_squared * ens_avg_R_squared) / k)
-                      << std::endl;
+            sum_R_squared += ThisWalk;
+            sum_R_squared_squared += ThisWalk * ThisWalk;
+            k++;
+            if (k % 10 == 0) // output every tenth step
+            {
+                const double ens_avg_R_squared{sum_R_squared / k};
+                std::cout << k << "\t"
+                        << ens_avg_R_squared << "\t"
+                        << std::sqrt(((sum_R_squared_squared / k)
+                                        - ens_avg_R_squared * ens_avg_R_squared) / k)
+                        << std::endl;
+            }
         }
     }
 }
@@ -129,15 +136,22 @@ void RunExp2(const int N, const int M, const int seed,
     {
         double sum_R_squared{0.0};
         double sum_R_squared_squared{0.0};
-        for (auto k= 1; k <= M; k++)
+        int k{1};
+        int seed_shift{0};
+        while (k < M)
         {
-            const double ThisWalk{RandomWalk(i, seed + k,
+            const double ThisWalk{RandomWalk(i, seed + seed_shift,
                                             self_avoid,
                                             spherical,
                                             segment_length,
                                             r)};
-            sum_R_squared += ThisWalk;
-            sum_R_squared_squared += ThisWalk * ThisWalk;
+            seed_shift++;
+            if (ThisWalk > 0)
+            {
+                sum_R_squared += ThisWalk;
+                sum_R_squared_squared += ThisWalk * ThisWalk;
+                k++;
+            }
         }
         const double ens_avg_R_squared{sum_R_squared / M};
         std::cout << i << "\t"
@@ -172,20 +186,11 @@ int main(int argc, char* argv[])
     const double r{atof(argv[7])};
     assert(segment_length >= r);
 
-    // const std::vector<double> a(3, 2.0), b(3, 3.0);
-    // std::cout << "Testing:\n"
+    RunExp1(N, M, seed, segment_length, self_avoid, spherical, r); // subtask a
 
-    //           << "AbsDist:" << AbsDistance(a, b) << "\n"
-    //           << "Overlap1:"<< Overlap(a, b, r) << std::endl;
-    RunExp1(N, M, seed, segment_length, self_avoid, spherical, r);
+    std::cout << "\n\n"; // separate outputs blocks for gnuplot
 
-    RunExp2(N, M, seed, segment_length, self_avoid, spherical, r);
-
-
-    // for (auto k=0; k < M; k++)
-    // {
-    //     std::cout << RandomWalk(N, seed + k, self_avoid, spherical, segment_length, r) << std::endl;
-    // }
+    RunExp2(N, M, seed, segment_length, self_avoid, spherical, r); // subtask b
 
     return 0;
 }
