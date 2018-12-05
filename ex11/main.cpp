5e337d27a84f679a98bdd873bcc1126dd89f29a7
BeatHubmann <BeatHubmann@users.noreply.github.com>
2018-12-05 13:52:46 +0100

    Initial work on ex11

---
+++ ex11/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <Eigen/Sparse>
+#include <Eigen/Cholesky>
+#include <cmath>
+#include "writer.hpp"
+
+using Real= double;
+using SparseMatrix= Eigen::SparseMatrix<Real>;
+using Triplet= Eigen::Triplet<Real>;
+using Vector= Eigen::VectorXd;
+
+
+void CreatePoissonMatrid()
+{
+
+}
+
+
+void CreateRHSVector()
+{
+
+}
+
+
+Vector SolvePoisson()
+{
+    Vector x;
+
+
+    return x;
+}
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+
+
+
+
+
+
+    return 0;
+}
